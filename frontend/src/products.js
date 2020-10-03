97bc30d6db6151022a4711a3d2d11a1ede1df2b3
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-03 10:57:19 +0100

    add Products component and dummy data

---
+++ frontend/src/products.js
@@ -0,0 +1,83 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Bar at the Folies Bergere',
+    image: '/images/A Bar at the Folies-Bergere-Manet.jpg',
+    description:
+      'Painting by Manet depicting a scene in the Folies Bergère nightclub in Paris. (1882).',
+    brand: 'Manet',
+    category: 'French Modernist',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Asakusa Rice Field During the Cock Festival at Otori Shrine.',
+    image:
+      '/images/Asakusa_Rice_Field_During_the_Cock_Festival_at_Otori_Shrine-Hokusai.jpg',
+    description:
+      'Ukiyo-e colour woodblock print by Japanese Artist Hokusai. (1857)',
+    brand: 'Hokusai',
+    category: 'Ukiyo-e',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Jas de Bouffan, The Pool-Cezanne',
+    image: '/images/Jas_de_Bouffan_The_Pool-Cezanne.jpg',
+    description:
+      'Painting of the Jas de Bouffan was the estate on the outskirts of Aix-en-Provence by French Post-Impressionist artist Paul Cezanne (1857)',
+    brand: 'Cezanne',
+    category: 'Post Impressionist',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Lady with an Ermine',
+    image: '/images/Lady_with_an_Ermine-DaVinci.jpg',
+    description:
+      'One of only four paintings of women by renaissance artist Leonardo Da Vinci. (1489–1490)',
+    brand: 'Da Vinci',
+    category: 'Renaissance',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'The Great Wave Off Kanagawa',
+    image: '/images/The_Great_Wave_Off_Kanagawa-Hokusai.jpg',
+    description:
+      'Colour woodblock print of a great wave threatening boats off the coast in the Sagami Bay by Japanese Artist Hokusai. (1829-1833)',
+    brand: 'Hokusai',
+    category: 'Ukiyo-e',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'The Luncheon of the Boating Party',
+    image: '/images/The_Luncheon_of_the_Boating_Party-Renoir.jpg',
+    description:
+      'Paintin by Renoir of a group of his friends relaxing on a balcony at the Maison Fournaise restaurant by the Seine. (1881)',
+    brand: 'Renoir',
+    category: 'Impressionist',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
