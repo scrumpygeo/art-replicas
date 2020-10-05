ab8e4e96353812264b3161f745842ca716b746a6
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-05 16:57:19 +0100

    add product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,70 @@
+import mongoose from 'mongoose'
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  { timestamps: true }
+)
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    dscription: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    // in mongoose we can pass in 2nd arg of options for created_at stuff:
+    timestamps: true,
+  }
+)
+
+//  create model from the schema called user
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
