5414b3de182d6541623aaf5ac6f1504e508c104c
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-08 15:36:20 +0100

    add card actions and reducers

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { CART_ADD_ITEM } from '../constants/cartConstants'
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`api/products/${id}`)
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty, // this comes from passed-in qty param at top
+    },
+  })
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
