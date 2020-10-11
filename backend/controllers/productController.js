b41ebe5d4b3bfb477309ee94bfd92d3dd430912a
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-11 14:51:18 +0100

    add user authentication endpoint

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// @descr Fetch All Products
+// @route GET /api/products
+// @access Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({})
+  res.json(products)
+})
+
+// @descr Fetch Single Product
+// @route GET /api/products/:id
+// @access Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404) // this line optional; without it it will just throw 500 error
+    throw new Error('Product not found')
+  }
+})
+
+export { getProducts, getProductById }
