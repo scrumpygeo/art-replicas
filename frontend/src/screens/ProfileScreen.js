eb39632ceda88c5d251f80d5c5dfbace8576f705
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-20 15:43:55 +0100

    make btn smaller in profile screen

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -148,7 +148,9 @@ const ProfileScreen = ({ location, history }) => {
                   </td>
                   <td>
                     <LinkContainer to={`/order/${order._id}`}>
-                      <Button variant='light'>Details</Button>
+                      <Button className='btn-sm' variant='light'>
+                        Details
+                      </Button>
                     </LinkContainer>
                   </td>
                 </tr>
