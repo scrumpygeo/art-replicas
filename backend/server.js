47ff6aa5e604d387fa10150f333c86edaf5a6044
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-20 14:01:27 +0100

    add paypal payments

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,10 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
 // middleware to handle errors
 // i. handles going to non existent route
 app.use(notFound)
