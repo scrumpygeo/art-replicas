350071897dc0ab6bef56cb0c4b1c49bad5c8ec62
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-24 15:18:59 +0100

    admin: create product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,22 +38,21 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 })
 
-// @descr   Create a Product
+// @desc    Create a product
 // @route   POST /api/products
 // @access  Private/Admin
 const createProduct = asyncHandler(async (req, res) => {
   const product = new Product({
-    name: 'Sample Name',
+    name: 'Sample name',
     price: 0,
     user: req.user._id,
     image: '/images/sample.jpg',
-    brand: 'Sample Artist',
+    brand: 'Sample artist',
     category: 'Sample category',
     countInStock: 0,
     numReviews: 0,
     description: 'Sample description',
   })
-
   const createdProduct = await product.save()
   res.status(201).json(createdProduct)
 })
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,12 +9,10 @@ import {
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
 router
   .route('/:id')
-  .get(getProducts)
-  .post(protect, admin, createProduct)
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
   .put(protect, admin, updateProduct)
