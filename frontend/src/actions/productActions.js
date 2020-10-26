cf77593f75be3e347c363e40ca3e0e1c320836de
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-26 16:05:22 +0000

    Admin: update product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -13,6 +13,9 @@ import {
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
+  PRODUCT_UPDATE_REQUEST,
 } from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
@@ -120,6 +123,46 @@ export const createProduct = () => async (dispatch, getState) => {
     dispatch({
       // here payload is the error
       type: PRODUCT_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.response,
+    })
+  }
+}
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_UPDATE_REQUEST,
+    })
+
+    const {
+      // userInfo - so we can get token from state easily by destructuring
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(
+      `/api/products/${product._id}`,
+      product,
+      config
+    )
+
+    dispatch({
+      type: PRODUCT_UPDATE_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      // here payload is the error
+      type: PRODUCT_UPDATE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
