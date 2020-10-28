25dfa67585825eb0529fac16c2e3e2da9e1ed0f6
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-28 13:37:09 +0000

    product reviews - front end

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ import {
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
+  productReviewCreateReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import {
@@ -32,7 +33,8 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
-  productUpdate: productUpdateReducer,
+  productUpdatecreate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
