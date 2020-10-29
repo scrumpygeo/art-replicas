81000e6b45c7d0a0fb080ed11f4bc8a7c84ff83f
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-29 17:21:22 +0000

    add custom page headers and meta

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Welcome To Art Replicas',
+  description: 'Access to world famous art',
+  keywords: 'art, paintings, old masters, portraits',
+}
+
+export default Meta
