c24d6b998f1120679da48ea37db1168cb6fcc778
scrumpygeo <scrumpygeo@yahoo.ca>
2020-10-30 11:22:07 +0000

    run build for frontend

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 app.use('/api/products', productRoutes)
 
 app.use('/api/users', userRoutes)
@@ -40,6 +36,19 @@ app.get('/api/config/paypal', (req, res) =>
 // make 'uploads' folder static so it's accessible from front end
 const __dirname = path.resolve() // method below only works with common js not with require so this line lets u mimic common js
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+// for deployment:
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 // middleware to handle errors
 // i. handles going to non existent route
---
+++ frontend/build/precache-manifest.4596574bb270b8b2f9fde2bc5842867c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c9345b6b332baadd391b4375d7affc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c12657ab9d5c41feef1",
+    "url": "/static/css/main.be978870.chunk.css"
+  },
+  {
+    "revision": "7fd4ff0ca337cf1dc2bf",
+    "url": "/static/js/2.717e9027.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.717e9027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c12657ab9d5c41feef1",
+    "url": "/static/js/main.eab9f9b3.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
